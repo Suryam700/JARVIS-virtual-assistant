9701f88a65c47ecab2037adad7077e0191ae70ba
Suryam700 <suryamgahoi14@gmail.com>
2025-11-06 13:46:23 +0530

    add more feature for more functionality

--- jarvis.py
+++ jarvis.py
@@ -1,41 +1,61 @@
 import speech_recognition as sr
-# now we can write speech-recognition as sr(in short form)
-import webbrowser # it's a builtin module no need to download this module form pip
-import pyttsx3 # text --> speech
+import webbrowser
+import pyttsx3 
+import musicLibrary
 
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
+newsApi = "2eaa023f581743929f7ccc6509c11272"
+country = "us"
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-# def processCommand():
-#     pass
+def processCommand(c):
+    if "open facebook" in c.lower():
+        webbrowser.open("http://www.facebook.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://www.youtube.com/")
+    elif "open google" in c.lower():
+        webbrowser.open("https://www.google.com/")
+    elif "open instagram" in c.lower():
+        webbrowser.open("https://www.instagram.com/")
+    elif "open whatsapp" in c.lower():
+        webbrowser.open("https://web.whatsapp.com/")
+    elif "open linkedin" in c.lower():
+        webbrowser.open("https://www.linkedin.com/")
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        webbrowser.open(musicLibrary.music[song])
+    elif "news" in c.lower():
+        webbrowser.open(f"https://newsapi.org/v2/top-headlines?country={country}&apiKey={newsApi}")
 
 if __name__ == "__main__": 
     speak("Initializing Jarvis...")
     while True:
-        # obtain audio from the microphone
         r = sr.Recognizer()
 
-        # recognize speech using sphinx
         print("Recognizing...")
         try:
             with sr.Microphone() as source:
                 print("Listening...")
-                audio = r.listen(source, timeout=2, phrase_time_limit=1)
+                r.adjust_for_ambient_noise(source)
+                audio = r.listen(source, timeout=3, phrase_time_limit=3)
             word = r.recognize_google(audio) 
             print(word)
-            # listen when someone says "Jarvis"
-            if(word.lower() == "jarvis"):
+            if "jarvis" in word.lower():
+                print("Condition matched")
                 speak("Yaa")
-                # listen for command
                 with sr.Microphone() as source:
                     print("Jarvis activated...")
                     audio = r.listen(source) 
-                    command = r.recognize_google(audio) 
-                    # processCommand()
+                    command = r.recognize_google(audio)
+
+                    processCommand(command) 
 
         except Exception as e:
             print("Error; {0}".format(e))
+
+
+
---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "north": "https://www.youtube.com/watch?v=DM7EVwMQ4qo&list=RDDM7EVwMQ4qo&start_radio=1",
+    "rush": "https://www.youtube.com/watch?v=19Iuib716m0",
+    "behavior": "https://www.youtube.com/watch?v=Jx02XQTz2LU ",
+    "virus": "https://www.youtube.com/watch?v=kBT4APYsP5M&list=PLzCxunOM5WFJmDRIzZJQexQfSxphkbB9o&index=2"
+}
